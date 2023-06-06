9c519128c8f4f8e3c7c89b866432b978d8fde869
kevin <kevin0x00@163.com>
2023-06-06 15:24:16 +0800

    customize lualine confiuration

--- lua/plugin_config/lualine.lua
+++ lua/plugin_config/lualine.lua
@@ -4,21 +4,46 @@
 vim.opt.showmode = false
 local enable_global_status = true
 
+local function my_date()
+    return os.date('%Y-%m-%d %H:%M:%S')
+end
+
+local function my_day_of_week()
+    local days = { 'Sun.', 'Mon.', 'Tues.', 'Wed.', 'Thur.', 'Fri.', 'Sat.' }
+    return days[os.date('%w') + 1]
+end
+
+-- constomize theme
+local custom_horizon = require'lualine.themes.horizon'
+
+custom_horizon.visual.a.bg = '#c893eb'
+custom_horizon.replace.a.bg = '#f1fa8d'
+
 require("lualine").setup({
     options = {
         icons_enabled = true, 
-        theme = "horizon",
-        section_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' },
+        theme = custom_horizon,
+        section_separators = { left = '', right = '' },
+        component_separators = { left = '', right = '' },
         globalstatus = enable_global_status,
+        always_divided_middle = false
     },
     sections = {
+        lualine_a = {
+                'mode'
+        },
         lualine_b = {
             {
-                'branch'
+                'branch',
+                colored = true,
+                color = { fg ='#00d91a' },
+                icons_enabled = true,
+                icon = { '', aligh = 'left', color = { fg = 'yellow' } }
             },
             {
-                'diff'
+              'diff',
+              colored = true, -- Displays a colored diff status if set to true
+              symbols = {added = ' ', modified = ' ', removed = ' '} -- Changes the symbols used by the diff.
             },
             {
                 'diagnostics',
@@ -26,8 +51,10 @@ require("lualine").setup({
                 symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
                 always_visible = false
             }
-        }
+        },
+        lualine_c = { { "require'lsp-status'.status()" }, { 'filename', colored = true, color = { fg = '#d0d0d0' } } },
+        lualine_y = { 'filesize', 'progress', 'location' },
+        lualine_z = { my_date }
     }
 })
-
 
