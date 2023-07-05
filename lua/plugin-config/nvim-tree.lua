f1e7ebb5d717d4642710185278461405200fa7ed
kevin <kevin0x00@163.com>
2023-07-05 22:38:32 +0800

    let nvim-tree show files in .gitignore

--- lua/plugin-config/nvim-tree.lua
+++ lua/plugin-config/nvim-tree.lua
@@ -1,8 +1,5 @@
 -- nvim-tree.lua
 
-
-
-
 local options = {
   on_attach = require 'user.keymaps'.nvim_tree(nil),
   -- filters = {
@@ -21,7 +18,18 @@ local options = {
   view = {
     adaptive_size = false,
     side = "left",
-    -- float = { enable = true, open_win_config = { border = "shadow" } },
+    float = {
+      enable = false,
+      quit_on_focus_loss = true,
+      open_win_config = {
+        relative = "editor",
+        border = "rounded",
+        width = 30,
+        height = 30,
+        row = 1,
+        col = 1,
+      },
+    },
     width = 30,
     preserve_window_proportions = true,
   },
@@ -76,6 +84,30 @@ local options = {
         },
       },
     },
+  },
+  diagnostics = {
+    enable = true,
+    show_on_dirs = false,
+    show_on_open_dirs = true,
+    debounce_delay = 50,
+    severity = {
+      min = vim.diagnostic.severity.HINT,
+      max = vim.diagnostic.severity.ERROR,
+    },
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
+  },
+  git = {
+    enable = true,
+    ignore = false,
+    show_on_dirs = true,
+    show_on_open_dirs = true,
+    disable_for_dirs = {},
+    timeout = 400,
   },
 }
 
