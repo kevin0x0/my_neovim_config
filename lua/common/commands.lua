6556f83ab93441d0c7233ae114c2d97a189a8478
kevin <kevin0x00@163.com>
2025-02-05 22:46:17 +0800

    improve Config command

--- lua/common/commands.lua
+++ lua/common/commands.lua
@@ -1,19 +1,39 @@
 -- my_command.lua
 
+local saved = nil
+
+local function validate_bufnr(bufnr)
+  if bufnr == -1 then
+    return 0
+  end
+  return bufnr
+end
+
 vim.api.nvim_create_user_command(
-  "MyConfig",
+  "Config",
   function(arg)
     --- @type string
     local configpath = vim.fn.stdpath('config');  --- @diagnostic disable-line
     local configfile = vim.fs.joinpath(configpath, "init.lua");
-    if arg.args == 'edit' then
+    if #arg.fargs == 0 then
+      saved = {
+        prev = saved,
+        dir = vim.fn.getcwd(),
+        bufnr = validate_bufnr(vim.fn.bufnr(0))
+      }
       vim.cmd.edit(configfile)
       vim.api.nvim_set_current_dir(configpath);
-    elseif arg.args == 'view' then
-      vim.cmd.edit(configfile)
+    elseif arg.fargs[1] == 'finish' then
+      if not saved then
+        vim.notify("you are not editing config", vim.log.levels.ERROR);
+        return
+      end
+      vim.api.nvim_set_current_buf(saved.bufnr)
+      vim.api.nvim_set_current_dir(saved.dir)
+      saved = saved.prev
     else
-      vim.notify("unknown option: " .. arg.args, vim.log.levels.ERROR);
+      vim.notify("unknown option: " .. arg.fargs[1], vim.log.levels.ERROR);
     end
   end,
-  { nargs = 1 }
+  { nargs = '?' }
 )
